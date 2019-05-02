577dc7e75f4898dfc6d02807b7063e10dec22911
zhalker <willardh.knox@gmail.com>
2019-05-02 16:20:08 +0100

    error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.state = {
       posts: []
     }
-    this.teste = this.teste.bind(this);
+    this.teste = this.teste.bind(tis);
   }
 
   getPosts = () => {
